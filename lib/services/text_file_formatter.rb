9b360221964f04d18b3a695c86fcd00002546033
Simon Denier <simon.denier@gmail.com>
2012-11-08 20:40:06 +0100

    Output team legs in a text file

--- lib/services/text_file_formatter.rb
+++ lib/services/text_file_formatter.rb
@@ -1,8 +1,23 @@
 class TextFileFormatter
 
   def output(filename, teams)
-    puts File.basename(filename, '.xml')
-    puts teams
+    File.open(outputName(filename), 'w') do |f|
+      teams.each {|team| teamOutput(team, f) }
+    end
+  end
+
+  def outputName(filename)
+    File.basename(filename, '.xml') + '.txt'
+  end
+
+  def teamOutput(team, file)
+    team.legs.each do |leg|
+      file.write legDescription(team, leg)
+    end
+  end
+
+  def legDescription(team, leg)
+    "#{team.bibNumber}.#{leg.legNo}: #{leg.courseVariation}\n"
   end
 
 end
